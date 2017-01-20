a64137dcfad3ea97aab6f0d1e7fb2508b2a7f0db
Ben-P-Leda <benp@ledaentertainment.co.uk>
2017-01-20 23:47:15 +0000

    Add box & health script

---
+++ Assets/Scripts/Destroyable Box/BoxHealth.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace DestroyableBox
+{
+    public class BoxHealth : MonoBehaviour
+    {
+        public int HitsToDestroy;
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            if (collider.tag == "Damage Collider")
+            {
+                HitsToDestroy -= 1;
+                if (HitsToDestroy < 1)
+                {
+                    HandleDestruction();
+                }
+            }
+        }
+
+        private void HandleDestruction()
+        {
+            // Just switch it off for now...
+            gameObject.SetActive(false);
+        }
+    }
+}
