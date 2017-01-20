48b431aa57d34416b1dd37f6a434b1edac5bc6e8
Ben-P-Leda <benp@ledaentertainment.co.uk>
2017-01-20 22:52:37 +0000

    Camera & input setup, input test

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        private Transform _transform;
+        private Rigidbody _rigidbody;
+
+        public string AxisPrefix;
+        public Vector3 InputVector;
+        public bool Fire1;
+        public bool Fire2;
+
+        private void Start()
+        {
+            _transform = transform;
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            InputVector = new Vector3(
+                Input.GetAxis(AxisPrefix + "-Horizontal"), 0.0f, Input.GetAxis(AxisPrefix + "-Vertical"));
+
+            if (Input.GetButtonDown(AxisPrefix + "-Fire1")) { Debug.Log(AxisPrefix + "Fire1"); }
+            if (Input.GetButtonDown(AxisPrefix + "-Fire2")) { Debug.Log(AxisPrefix + "Fire2"); }
+        }
+    }
+}
