a783f5e13b82c1fee91d1120727c265ea1cd14c1
Ben-P-Leda <benp@ledaentertainment.co.uk>
2017-01-20 23:20:48 +0000

    "Rotation" control

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -23,7 +23,11 @@ namespace Player
 
         private void HandleRotation()
         {
+            float stickValue = Input.GetAxis(AxisPrefix + "-Horizontal");
+            float delta = Mathf.Abs(stickValue) > Rotation_Threshold ? stickValue : 0.0f;
+            float updatedY = _transform.eulerAngles.y + (delta * Maximum_Rotation_Speed);
 
+            _transform.localRotation = Quaternion.Euler(_transform.eulerAngles.x, updatedY, _transform.eulerAngles.z);
         }
 
         private void HandleThrust()
@@ -34,6 +38,8 @@ namespace Player
             _rigidbody.velocity = new Vector3(delta.x, _rigidbody.velocity.y, delta.z);
         }
 
+        private const float Rotation_Threshold = 0.25f;
+        private const float Maximum_Rotation_Speed = 2.0f;
         private const float Maximum_Movement_Speed = 5.0f;
     }
 }
