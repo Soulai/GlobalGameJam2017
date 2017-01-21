bf9c95997a303e65daf00020ce59b1e44ab95135
Ben-P-Leda <benp@ledaentertainment.co.uk>
2017-01-21 09:28:57 +0000

    Exit gate opens when both players nearby

--- Assets/Scripts/Exit Gate/ExitTrigger.cs
+++ Assets/Scripts/Exit Gate/ExitTrigger.cs
@@ -4,5 +4,40 @@ namespace ExitGate
 {
     public class ExitTrigger : MonoBehaviour
     {
+        private Animator _animator;
+
+        private int _playersInProximity;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.transform.tag == "Player")
+            {
+                _playersInProximity += 1;
+                if (_playersInProximity == 2)
+                {
+                    TriggerLevelClearedSequence();
+                }
+            }
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.transform.tag == "Player")
+            {
+                _playersInProximity -= 1;
+            }
+        }
+
+        private void TriggerLevelClearedSequence()
+        {
+            _animator.SetTrigger("Open");
+
+            // TODO: Fire an event that ends the round
+        }
     }
 }
