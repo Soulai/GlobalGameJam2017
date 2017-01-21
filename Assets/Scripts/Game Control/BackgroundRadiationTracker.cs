fa6767b33bdadae08dc8bf92e455f12e3ed1b4d5
Ben-P-Leda <benp@ledaentertainment.co.uk>
2017-01-21 12:15:04 +0000

    Couple of fixes

--- Assets/Scripts/Game Control/BackgroundRadiationTracker.cs
+++ Assets/Scripts/Game Control/BackgroundRadiationTracker.cs
@@ -6,20 +6,20 @@ namespace GameControl
     {
         private float _levelTimeElapsed;
 
-        public float TimeToRadiationPeak;
+        [SerializeField]
+        private float _timeToRadiationPeak = 20.0f;
 
         public float BackgroundRadiationLevel; // { get; private set; }
 
         private void Start()
         {
-            TimeToRadiationPeak = TimeToRadiationPeak <= 0.0f ? 20.0f : TimeToRadiationPeak;
             _levelTimeElapsed = 0.0f;
         }
 
         private void Update()
         {
             _levelTimeElapsed += Time.deltaTime;
-            BackgroundRadiationLevel = Maximum_Background_Radiation * Mathf.Clamp01(_levelTimeElapsed / TimeToRadiationPeak);
+            BackgroundRadiationLevel = Maximum_Background_Radiation * Mathf.Clamp01(_levelTimeElapsed / _timeToRadiationPeak);
         }
 
         private const float Maximum_Background_Radiation = 5.0f;
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -5,22 +5,18 @@ namespace Player
 {
     public class PlayerHealth : MonoBehaviour
     {
-        public float CurrentHealth; //{ get; private set; }
+        public float CurrentHealth;
 
         private BackgroundRadiationTracker _backgroundRadiationTracker;
 
         private void Start()
         {
             _backgroundRadiationTracker = FindObjectOfType<BackgroundRadiationTracker>();
-
-            CurrentHealth = Initial_Health;
         }
 
         public void Update()
         {
             CurrentHealth = Mathf.Max(0.0f, CurrentHealth - _backgroundRadiationTracker.BackgroundRadiationLevel);
         }
-
-        private const float Initial_Health = 10000.0f;
     }
 }
