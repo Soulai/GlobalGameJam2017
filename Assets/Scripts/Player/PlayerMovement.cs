ec77ad096b82c3496004293191ad83bade1a3608
Ben-P-Leda <benp@ledaentertainment.co.uk>
2017-01-21 00:39:06 +0000

    Stop movement when swing triggered

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,7 @@ namespace Player
         private Transform _transform;
         private Rigidbody _rigidbody;
         private Animator _animator;
+        private bool _swingInProgress;
 
         public string AxisPrefix;
 
@@ -15,19 +16,27 @@ namespace Player
             _transform = transform;
             _rigidbody = GetComponent<Rigidbody>();
             _animator = GetComponentInChildren<Animator>();
+
+            _swingInProgress = false;
         }
 
         private void Update()
         {
             HandleRotation();
             HandleThrust();
+
+            if ((Input.GetButtonDown(AxisPrefix + "-Fire1")) && (!_swingInProgress))
+            {
+                _swingInProgress = true;
+            }
         }
 
         private void HandleRotation()
         {
             float stickValue = Input.GetAxis(AxisPrefix + "-Horizontal");
-            float delta = Mathf.Abs(stickValue) > Rotation_Threshold ? stickValue : 0.0f;
-            float updatedY = _transform.eulerAngles.y + (delta * Maximum_Rotation_Speed);
+
+            stickValue = Mathf.Abs(stickValue) > Rotation_Threshold ? stickValue : 0.0f;
+            float updatedY = _transform.eulerAngles.y + (stickValue * Maximum_Rotation_Speed);
 
             _transform.localRotation = Quaternion.Euler(_transform.eulerAngles.x, updatedY, _transform.eulerAngles.z);
         }
@@ -35,11 +44,16 @@ namespace Player
         private void HandleThrust()
         {
             float stickValue = Input.GetAxis(AxisPrefix + "-Vertical");
-            float delta = Mathf.Abs(stickValue) > Movement_Threshold ? stickValue : 0.0f;
-            Vector3 planarVector = new Vector3(_transform.forward.x, 0.0f, _transform.forward.z);
-            Vector3 movementVector = planarVector.normalized * delta * Maximum_Movement_Speed;
 
-            _animator.SetBool("IsMoving", delta > Movement_Threshold);
+            if ((Mathf.Abs(stickValue) < Movement_Threshold) || (_swingInProgress))
+            {
+                stickValue = 0.0f;
+            }
+
+            Vector3 planarVector = new Vector3(_transform.forward.x, 0.0f, _transform.forward.z);
+            Vector3 movementVector = planarVector.normalized * stickValue * Maximum_Movement_Speed;
+
+            _animator.SetBool("IsMoving", stickValue > Movement_Threshold);
 
             _rigidbody.velocity = new Vector3(movementVector.x, _rigidbody.velocity.y, movementVector.z);
         }
