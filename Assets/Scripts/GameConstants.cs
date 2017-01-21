cbff62f4392562a911f0573cad6017186858ee17
Corentin Lemasson <lemasson.corentin@laposte.net>
2017-01-21 17:04:58 +0000

    Decoy behaviour (WIP)

--- Assets/Scripts/GameConstants.cs
+++ Assets/Scripts/GameConstants.cs
@@ -2,4 +2,6 @@
 {
 	public static string GAME_MANAGER_TAG = "GameManager";
 	public static string PATROL_POINT_TAG = "PatrolPoint";
+	public static string PLAYER_TAG = "Player";
+	public static string WALL_TAG = "Wall";
 }
