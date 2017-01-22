fa1e5f21213c74952b9592cf6e17550c042723fc
Corentin Lemasson <lemasson.corentin@laposte.net>
2017-01-22 15:42:21 +0000

    Add title screen

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour 
+{
+	public string sceneName;
+	public Button startGameButton;
+
+	private bool isLoading = false;
+
+	void Update()
+	{
+		startGameButton.Select();
+	}
+
+	public void OnStartGameButtonClicked()
+	{
+		if (!isLoading)
+		{
+			startGameButton.GetComponentInChildren<Text>().text = "Loading...";
+			SceneManager.LoadSceneAsync(sceneName);
+			isLoading = true;
+		}
+	}
+}
