47f232620c34c4cd886b1007a16d231bc30eb6f0
Ben-P-Leda <benp@ledaentertainment.co.uk>
2017-01-22 15:33:24 +0000

    More sequencing effects

---
+++ Assets/Scripts/SceneSwitchListener.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitchListener : MonoBehaviour
+{
+    public string NextSceneName;
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(NextSceneName);
+        }
+    }
+}
