fc4e876c880f48bb6a2fb612fd4de0de9be06f32
Corentin Lemasson <lemasson.corentin@laposte.net>
2017-01-22 14:56:11 +0000

    Light tweaks + menu (WIP)

---
+++ Assets/Scripts/RotateAroundTarget.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotateAroundTarget : MonoBehaviour 
+{
+	public float rotationSpeed = 5f;
+	public Transform target;
+
+	void Update()
+	{
+		transform.RotateAround(target.position, Vector3.up, rotationSpeed * Time.deltaTime);
+	}
+}
